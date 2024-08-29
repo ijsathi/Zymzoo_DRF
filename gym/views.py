3bb1f34a515130301b9a937e6b54f9ac53af242b
ijsathi <ijsathi2001rs@gmail.com>
2024-08-29 12:33:15 +0600

    add is_staff

--- gym/views.py
+++ gym/views.py
@@ -72,7 +72,11 @@ class UserLoginView(APIView):
             if user is not None:
                 login(request, user)
                 token, _ = Token.objects.get_or_create(user=user)
-                return Response({'token': token.key, 'user_id': user.id}, status=status.HTTP_200_OK)
+                return Response({
+                    'token': token.key, 
+                    'user_id': user.id, 
+                    'is_staff': user.is_staff  # Include is_staff in the response
+                }, status=status.HTTP_200_OK)
             else:
                 return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
